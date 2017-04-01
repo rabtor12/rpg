d4410b5cae69ae774829a7b75663123a6915b303
Sergey <raptor12r@gmail.com>
2017-04-01 22:29:27 +0300

    добавление Fireball

--- Assets/WanderingAL.cs
+++ Assets/WanderingAL.cs
@@ -2,6 +2,8 @@
 
 public class WanderingAL : MonoBehaviour
 {
+    [SerializeField] private GameObject Fireball;
+    private GameObject _fireball;
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     private bool _alive;
@@ -19,11 +21,23 @@ public class WanderingAL : MonoBehaviour
             var ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit))
-                if (hit.distance < obstacleRange)
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(Fireball) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward*1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+               else if (hit.distance < obstacleRange)
                 {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
                 }
+            }
         }
     }
 
